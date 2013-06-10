7e218bdf768446ab5ba6466016521b0ef577df4e
Carlos Tejada <tejada_ca@hotmail.com>
2013-06-10 05:03:30 -0400

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Secondblog::Application.config.secret_token = 'dab064d06caf2147b3ecbc8e16e725472fc41073a07b54a9197672be7fff02808ec5226249fe0797c5267cdadc986dd828dd641304fbec83e42d7e98d860cdc2'
